2e3e087373a0c273c7cf158c186c1d30ad75f582
M'bark Erras <c@mathematikoi.xyz>
2024-04-11 23:01:56 +0000

    fix: draft-07 ftw, dialect not recognized cuz https instead of http

--- bundle.js
+++ bundle.js
@@ -4,33 +4,39 @@ const path = require("path");
 const schemas_directory_path = path.join(__dirname, "source/schemas");
 
 async function bundle() {
-  const registry = await import("@hyperjump/json-schema/draft-2020-12");
+  const dialect = await import("@hyperjump/json-schema/draft-07");
 
   const bundler = await import("@hyperjump/json-schema/bundle");
 
   const schemas = fs
     .readdirSync(schemas_directory_path)
-    .filter((file) => file.endsWith(".json") && !file.endsWith(".bundled.json"));
+    .filter(
+      (file) => file.endsWith(".json") && !file.endsWith(".bundled.json")
+    );
   schemas.forEach(async (schema_file) => {
     const schema_path = path.join(schemas_directory_path, schema_file);
 
     const schema = require(schema_path);
 
-    const uri = `https://schemas.hyper.mathematikoi.co/${schema_file}`
+    const uri = `https://schemas.hyper.mathematikoi.co/${schema_file}`;
 
-    registry.registerSchema(
-      schema,
-      uri
-    );
+    dialect.registerSchema(schema, uri);
 
-    const bundled = await bundler.bundle(uri);
+    // try {
+    //   const output = await dialect.validate(uri);
+    //   console.log({ output });
+    // } catch (error) {
+    //   console.log(error.output);
+    // }
 
-    const bundled_schema_path = path.join(
-      schemas_directory_path,
-      schema_file.replace(".json", ".bundled.json")
-    );
+    // const bundled = await bundler.bundle(uri);
 
-    fs.writeFileSync(bundled_schema_path, JSON.stringify(bundled, null, 2));
+    // const bundled_schema_path = path.join(
+    //   schemas_directory_path,
+    //   schema_file.replace(".json", ".bundled.json")
+    // );
+
+    // fs.writeFileSync(bundled_schema_path, JSON.stringify(bundled, null, 2));
   });
 }
 
