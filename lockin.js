dc269f109795df8dd5e7137f45546b8e7e31f555
M'bark Erras <c@mathematikoi.xyz>
2024-04-18 18:31:19 +0100

    wip

--- lockin.js
+++ lockin.js
@@ -3,32 +3,33 @@ const path = require("path");
 const { compile } = require("json-schema-to-typescript");
 const schemas_directory_path = path.join(__dirname, "source/schemas");
 
-
 async function lockin() {
   const schemas = fs.readdirSync(schemas_directory_path);
 
-  schemas.forEach(async (schema_file, index) => {
-    const name = schema_file.split("-").at(0);
+  schemas
+    .filter((schema_file) => schema_file.endsWith(".json"))
+    .forEach(async (schema_file, index) => {
+      const name = schema_file.split("-").at(0);
 
-    const main_file_path = path.join(__dirname, "types", `${name}.d.ts`)
+      const main_file_path = path.join(__dirname, "types", `${name}.d.ts`);
 
-    if (fs.existsSync(main_file_path)) {
-      fs.unlinkSync(main_file_path);
-    }
-    
-    const schema_path = path.join(schemas_directory_path, schema_file);
-    
-    const schema = require(schema_path)
+      if (fs.existsSync(main_file_path)) {
+        fs.unlinkSync(main_file_path);
+      }
 
-    schema.$id = name
+      const schema_path = path.join(schemas_directory_path, schema_file);
 
-    try {
-      const type = await compile(schema, name);
-      fs.writeFileSync(main_file_path, type);
-    } catch (error) {
-      console.log(name, error);
-    }
-  });
+      const schema = require(schema_path);
+
+      schema.$id = name;
+
+      try {
+        const type = await compile(schema, name);
+        fs.writeFileSync(main_file_path, type);
+      } catch (error) {
+        console.log(name, error);
+      }
+    });
 }
 
 lockin();
